e8a0aa45b021939f8582d701d17b65ddbe94b2ea
Hacnguyet77 <146824919+Hacnguyet77@users.noreply.github.com>
2023-10-20 16:33:09 +0700

    push du an

---
+++ web ban gau bong/DoAn_Web1bangaubong/data/products.js
@@ -0,0 +1,342 @@
+var list_products = [{
+    "name": " gau teddy",
+    "phan_loai": "gau bong",
+    "img": "img/products/gau-teddy.jpg",
+    "price": "120.000",
+    "star": 3,
+    "rateCount": 26,
+    "promo": {
+        "name": "giamgia",
+        "value": "20.000"
+    },
+    "detail": {
+        "size": "1m",
+        "color":"vang",
+        "quantity":"22"
+    },
+    "masp": "gau0"
+}, {
+    "name": " gau teddy trang ao hong",
+    "phan_loai": "gau bong",
+    "img": "img/products/gau-teddy-trang-ao-hong.jpg",
+    "price": "245.000",
+    "star": 5,
+    "rateCount": 0,
+    "promo": {
+        "name": "giamgia",
+        "value": "5.000"
+    },
+    "detail": {
+        "size": "1m2",
+        "color":"vàng , hồng",
+        "quantity":"9"
+    },
+    "masp": "gau1"
+}, {
+    "name": "gau doi trang ",
+    "phan loai": "gau",
+    "img": "img/products/doi-trang.jpg",
+    "price": "320.000",
+    "star": 4.5,
+    "rateCount": 188,
+    "promo": {
+        "name": "giamgia",
+        "value": "20.000"
+    },
+    "detail": {
+        "size": "1m",
+        "color":"trắng",
+        "quantity":"55"
+    },
+    "masp": "gau2"
+}, {
+    "name": "goi bo vit",
+    "phan loai": "goi",
+    "img": "img/products/goi-om-bo-vit.jpg",
+    "price": "460.000",
+    "star": 5,
+    "rateCount": 7,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+       "size":"1m2",
+       "color":"hồng, xanh",
+       "quantity":"24"
+    },
+    "masp": "goi0"
+}, {
+    "name": "goi meo hoang thuong",
+    "phan loai": "goi",
+    "img": "img/products/goi-om-meo-ht.jpg",
+    "price": "790.000",
+    "star": 1,
+    "rateCount": 1,
+    "promo": {
+        "name": "giamgia",
+        "value": "40.000"
+    },
+    "detail": {
+        "size": "1m2",
+        "color":"",
+        "quantity":"44"
+    },
+    "masp": "goi1"
+}, {
+    "name": "goi co bong hong dau",
+    "phan_loai": "goi",
+    "img": "img/products/goi-co-ht-lotso.jpg",
+    "price": "150.000",
+    "star": 4,
+    "rateCount": 10,
+    "promo": {
+        "name": "giareonline",
+        "value": "120.000"
+    },
+    "detail": {
+        "size": "30cm",
+        "color": "hồng dâu",
+        "quantity":"33"
+    },
+    "masp": "goiu0"
+}, {
+    "name": "gau om tim tho",
+    "phan_loai": "gau bong",
+    "img": "img/products/teddy-om-tim-tho.jpg",
+    "price": "600.000",
+    "star": 4,
+    "rateCount": 40,
+    "promo": {
+        "name": "giareonline",
+        "value": "580.000"
+    },
+    "detail": {
+        "size": "1m6",
+        "color":"tím",
+        "quantity":"24"
+    },
+    "masp": "gau3"
+}, {
+    "name": "gau om tra sua",
+    "phan_loai": "gau bong",
+    "img": "img/products/gau-tra-sua.jpg",
+    "price": "800.000",
+    "star": 3,
+    "rateCount": 3,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "size": "1m2",
+        "color":"hồng, vàng",
+        "quantity":"12"
+    },
+    "masp": "gau4"
+}, {
+    "name": "gau bach tuoc cam xuc",
+    "phan_loai": "gau bong",
+    "img": "img/products/bach-tuoc-cam-xuc.jpg",
+    "price": "680.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "giareonline",
+        "value": "650.000"
+    },
+    "detail": {
+        "size": "1m",
+        "color":"hồng",
+        "quantity":"31"
+    },
+    "masp": "gau5"
+}, {
+    "name": " goi + mu[phu kien] ",
+    "phan_loai": "phu kien",
+    "img": "img/products/goi-co-mu-5.jpg",
+    "price": "290.000",
+    "star": 5,
+    "rateCount": 45,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "size": "35cm",
+        "color":" [ngẫu nhiên] hồng/tím",
+        "quantity":"27"
+    },
+    "masp": "pk0"
+}, {
+    "name": "goi tua stitch tim",
+    "phan_loai": "goi",
+    "img": "img/products/goi-tua-stitch-tim.jpg",
+    "price": "520.000",
+    "star": 4,
+    "rateCount": 16,
+    "promo": {
+        "name": "giamgia",
+        "value": "20.000"
+    },
+    "detail": {
+        "size": "62cm",
+        "color":"tím",
+        "quantity":"34"
+    },
+    "masp": "goi2"
+}, {
+    "name": " moc cho no tai [phu kien]",
+    "phan_loai": "phu kien",
+    "img": "img/products/moc-khoa-bts-mem.jpg",
+    "price": "30.000",
+    "star": 0,
+    "rateCount": 0,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "size": "10 cm",
+        "color":"vang",
+        "quantity":"15 "
+    },
+    "masp": "pk1"
+}, {
+    "name": "moc khoa shiba [phu kien ]",
+    "phan_loai": "phu kien",
+    "img": "img/products/moc-khoa-shiba.jpg",
+    "price": "40.000",
+    "star": 4,
+    "rateCount": 80,
+    "promo": {
+        "name": "",
+        "value": ""
+    },
+    "detail": {
+        "size": "10 cm",
+        "color":"...",
+        "quantity":"20"
+    },
+    "masp": "pk2"
+}, {
+    "name": "hop nhan bong[phu kien ]",
+    "phan_loai": "phu kien",
+    "img": "img/products/hop-nhan-bong.jpg",
+    "price": "190.000",
+    "star": 5,
+    "rateCount": 87,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "size": "",
+        "color":"",
+        "quantity":"39"
+    },
+    "masp": "pk3"
+}, {
+    "name": " bo hoa nhu 40 bong [phu kien]",
+    "phan_loai": "phu kien",
+    "img": "img/products/bo-hoa-nhu-40-bong.jpg",
+    "price": "1.090.000",
+    "star": 5,
+    "rateCount": 87,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "size": "40 bông",
+        "color":"hồng",
+        "quantity":"39"
+    },
+    "masp": "pk4"
+}, {
+    "name": "goi co mu",
+    "phan_loai": "goi",
+    "img": "img/products/Goi-co-2.jpg",
+    "price": "190.000",
+    "star": 4,
+    "rateCount": 8,
+    "promo": {
+        "name": "moiramat",
+        "value": ""
+    },
+    "detail": {
+        "size": "30 cm",
+        "color":"cam, xanh",
+        "quantity":"39"
+    },
+    "masp": "goiu1"}, {
+        "name": " cai toc den [phu kien]",
+        "phan_loai": "phu kien",
+        "img": "img/products/kep-toc-hoa-hong.jpg",
+        "price": "90.000",
+        "star": 4,
+        "rateCount": 8,
+        "promo": {
+            "name": "moiramat",
+            "value": ""
+        },
+        "detail": {
+            "size": "",
+            "color":"đen",
+            "quantity":"30"
+        },
+        "masp": "pk5"
+    }, {
+        "name": " bang do do [phu kien]",
+        "phan_loai": "phu kien",
+        "img": "img/products/bang-do-do.jpg",
+        "price": "70.000",
+        "star": 0,
+        "rateCount": 0,
+        "promo": {
+            "name": "giareonline",
+            "value": "65.000"
+        },
+        "detail": {
+            "size": "",
+            "color":"đỏ",
+            "quantity":"25"
+        },
+        "masp": "pk5"
+    },{
+        "name": "goi non tom",
+        "phan_loai": "goi",
+        "img": "img/products/goi-co-hinh-tom-4.jpg",
+        "price": "155.000",
+        "star": 3.5,
+        "rateCount": 20,
+        "promo": {
+            "name": "giareonline",
+            "value": "150.000"
+        },
+        "detail": {
+            "size": "",
+            "color":"cam",
+            "quantity":"10"
+        },
+        "masp": "goiu2"
+    },{
+        "name": "gau husky",
+        "phan_loai": "gau bong",
+        "img": "img/products/husky-mem-3.jpg",
+        "price": "905.000",
+        "star": 0,
+        "rateCount": 0,
+        "promo": {
+            "name": "giareonline",
+            "value": "900.000"
+        },
+        "detail": {
+            "size": "1m2",
+            "color":"xám",
+            "quantity":"15"
+        },
+        "masp": "gau6"
+    }
+
+]
